69c72bc6126c8256c5cfac19a2e8a87b2d1b26cd
YgorLourenco <44757992+YgorLourenco@users.noreply.github.com>
2021-04-24 22:59:53 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import {qs, qsa} from '/app/utils/dom.utils.js'
+import {randomNumber} from '/app/utils/math.util.js'
+
+const mainEl = qs('#main')
+const loadingEl = qsa('#loading')
+
+function startFakeLoading() {
+    mainEl.style.display = 'none'
+    loadingEl.style.display = ''
+
+    setTimeout(_ => {
+        mainEl.style.display = ''
+        loadingEl.style.display = 'none'
+    }, randomNumber(10, 5) * 100)
+}
+
+startFakeLoading()
